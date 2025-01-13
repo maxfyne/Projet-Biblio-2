2eb4fe17b483e3ab1b4ba74eb0ee24b9e901198b
unknown <fyne@220056slocal>
2025-01-13 17:15:02 +0100

    ajustement d'ajoutuser

--- accueil.php
+++ accueil.php
@@ -20,6 +20,7 @@ $_SESSION["cleLivre"]=1;
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="style.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
